039ef669564492ad42c54487ec4057a2afb40c18
Sergio Arias <s-andres140@hotmail.com>
2024-05-05 13:38:36 -0500

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,52 @@
+// Importa la biblioteca Mongoose
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://first_mongo_database:${password}@cluster0.hbnnnz3.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+
+// Crea nuevos usuarios
+if(process.argv.length===5){
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+else if(process.argv.length===3){
+  // Devuelve todas las instancias de person en la base de datos
+
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+
+
